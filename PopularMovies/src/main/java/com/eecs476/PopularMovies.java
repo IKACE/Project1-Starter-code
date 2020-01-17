fcac5b387289f84327f099aa6f49cdea32c4f73a
Carol <carol@Carols-MacBook-Pro.local>
2020-01-17 00:38:40 -0500

    change name

--- PopularMovies/src/main/java/com/eecs476/PopularMovies.java
+++ PopularMovies/src/main/java/com/eecs476/PopularMovies.java
@@ -1,8 +1,9 @@
 package com.eecs476;
 import java.io.*;
 
-public class PopularGenres {
+public class PopularMovies {
   //TODO Finish Part1 Q2 here
   public static void main(String[] args) {
+    System.out.println("Success");
   }
 }
